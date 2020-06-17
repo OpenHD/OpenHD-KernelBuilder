76f0096132c0ba7806aa7f50bef66b16888ffab7
Stephen Oliver <steve@infincia.com>
2020-06-16 20:39:15 -0400

    Remove old package if rebuilding

--- build.sh
+++ build.sh
@@ -151,6 +151,8 @@ package() {
 
     VERSION=$(git describe --tags | sed 's/\(.*\)-.*/\1/')
 
+    rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
+
     fpm -a ${PACKAGE_ARCH} -s dir -t deb -n ${PACKAGE_NAME} -v ${VERSION} -C ${PACKAGE_DIR} -p ${PACKAGE_NAME}_VERSION_ARCH.deb || exit 1
 
     #
