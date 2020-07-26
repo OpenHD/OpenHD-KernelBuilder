9e79b531471b87e0518ba777f48b83a774a7479e
LimitlessGeen <limitlessgreen@outlook.de>
2020-07-26 13:03:22 +0200

    Work arround ccache
    Because something went wrong inside the Realtek drivers

--- build.sh
+++ build.sh
@@ -43,11 +43,29 @@ mkdir -p ${PACKAGE_DIR}/usr/local/share/openhd/kernel/dtb || exit 1
 
 
 if [[ "${PLATFORM}" == "pi" ]]; then
+    PI_TOOLS_COMPILER_PATH="$(pwd)/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin"
     if [ ! "$(ls -A ${PWD}/tools)" ]; then
         echo "Downloading Raspberry Pi toolchain"
         git clone --depth=1 -b ${PI_TOOLS_BRANCH} ${PI_TOOLS_REPO} $(pwd)/tools
-        export PATH=$(pwd)/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin:${PATH}
+    fi
+    if [[ ${PATH} != *${PI_TOOLS_COMPILER_PATH}* ]]; then
+        export PATH=${PI_TOOLS_COMPILER_PATH}:${PATH}
         echo "Path: ${PATH}"
+    fi
+    # CCACHE workaround
+    CCACHE_PATH=${PI_TOOLS_COMPILER_PATH}/../bin-ccache
+    if [[ ! "$(ls -A ${CCACHE_PATH})" ]]; then
+        mkdir -p ${CCACHE_PATH}
+        pushd ${CCACHE_PATH}
+            ln -s $(which ccache) arm-linux-gnueabihf-gcc
+            ln -s $(which ccache) arm-linux-gnueabihf-g++
+            ln -s $(which ccache) arm-linux-gnueabihf-cpp
+            ln -s $(which ccache) arm-linux-gnueabihf-c++
+        popd
+        export PATH=${CCACHE_PATH}:${PATH}
+    fi
+    if [[ ${PATH} != *${CCACHE_PATH}* ]]; then
+        export PATH=${CCACHE_PATH}:${PATH}
     fi
 
     export ARCH=arm
