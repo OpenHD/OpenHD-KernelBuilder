9c53c7bc6458411ef822b407161dd0501a4f6bd0
LimitlessGreen <limitlessgreen@outlook.de>
2021-09-13 12:02:53 +0200

    Try newer RTL drivers

--- build.sh
+++ build.sh
@@ -13,11 +13,15 @@ DISTRO=$2
 RTL_8812AU_REPO=https://github.com/aircrack-ng/rtl8812au.git
 RTL_8812AU_BRANCH=v5.6.4.2
 
-RTL_8812BU_REPO=https://github.com/OpenHD/rtl88x2bu.git
-RTL_8812BU_BRANCH=5.6.1_30362.20181109_COEX20180928-6a6a
+#RTL_8812BU_REPO=https://github.com/OpenHD/rtl88x2bu.git
+#RTL_8812BU_BRANCH=5.6.1_30362.20181109_COEX20180928-6a6a
+RTL_8812BU_REPO=https://github.com/cilynx/rtl88x2bu.git
+RTL_8812BU_BRANCH=5.8.7.1_35809.20191129_COEX20191120-7777
 
-RTL_8188EUS_REPO=https://github.com/OpenHD/rtl8188eus.git
-RTL_8188EUS_BRANCH=openhd1
+#RTL_8188EUS_REPO=https://github.com/OpenHD/rtl8188eus.git
+#RTL_8188EUS_BRANCH=openhd1
+RTL_8188EUS_REPO=https://github.com/aircrack-ng/rtl8188eus.git
+RTL_8188EUS_BRANCH=v5.3.9
 
 V4L2LOOPBACK_REPO=https://github.com/OpenHD/v4l2loopback.git
 V4L2LOOPBACK_BRANCH=openhd2
