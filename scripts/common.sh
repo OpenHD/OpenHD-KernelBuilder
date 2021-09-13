9499ab17cae1d354cb66a7e13a743a6c88989496
LimitlessGreen <limitlessgreen@outlook.de>
2021-09-13 23:34:12 +0200

    reordering...

--- scripts/common.sh
+++ scripts/common.sh
@@ -24,7 +24,9 @@ function package() {
     rm ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb > /dev/null 2>&1
     
     echo "Package the kernel with fpm"
-    fpm -a ${PACKAGE_ARCH} --after-install after-install.sh --before-install before-install.sh -p ${PACKAGE_NAME}_VERSION_ARCH.deb -s dir -t deb --name ${PACKAGE_NAME} --version ${VERSION} --chdir ${PACKAGE_DIR}  || exit 1
+    fpm -a ${PACKAGE_ARCH} --after-install after-install.sh --before-install before-install.sh\
+    -p ${PACKAGE_NAME}_VERSION_ARCH.deb -s dir -t deb\
+    --version ${VERSION} --chdir ${PACKAGE_DIR} --name ${PACKAGE_NAME} || exit 1
 
     #
     # You can build packages and test them locally without tagging or uploading to the repo, which is only done for
