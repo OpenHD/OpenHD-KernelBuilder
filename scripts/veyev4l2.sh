1a4a5fb92b70388e21213b1e4b8e359dcd2afc2c
MacDaddyFPV <dmccullock83@gmail.com>
2022-04-20 23:03:24 +1000

    add veye v4l2 drivers

---
+++ scripts/veyev4l2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function fetch_veyev4l2_driver() {
+    if [[ ! "$(ls -A veyev4l2)" ]]; then    
+        echo "Download the veyev4l2 driver"
+        git clone ${VEYEV4L2REPO}
+    fi
+
+    pushd veyev4l2
+        git fetch
+        git reset --hard
+        git checkout ${VEYEV4L2_BRANCH}
+    popd
+
+}
+
+function build_veyev4l2_driver() {
+    pushd veyev4l2
+        install -p -m 644 ./pi/$(uname -r)/veyecam2m.ko  /lib/modules/$(uname -r)/kernel/drivers/media/i2c/
+        install -p -m 644 ./pi/$(uname -r)/veyecam2m.dtbo /boot/overlays/
+        install -p -m 644 ./pi/$(uname -r)/csimx307.ko  /lib/modules/$(uname -r)/kernel/drivers/media/i2c/
+        install -p -m 644 ./pi/$(uname -r)/csimx307.dtbo /boot/overlays/
+        /sbin/depmod -a $(uname -r)
+    popd
+}
