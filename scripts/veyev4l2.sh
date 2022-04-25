88eebe4c46cc1137b4d28d6bfee06cf9f7e86739
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-04-25 17:12:55 +0200

    remove unnecessary folders, add time to package

--- scripts/veyev4l2.sh
+++ scripts/veyev4l2.sh
@@ -16,9 +16,6 @@ function fetch_veyev4l2_driver() {
 
 function build_veyev4l2_driver() {
     pushd veyev4l2
-        mkdir -p ${PACKAGE_DIR}/lib/modules/5.10.92-v7+/kernel/drivers/media/i2c/
-        mkdir -p ${PACKAGE_DIR}/lib/modules/5.10.92-v7l+/kernel/drivers/media/i2c/
-        mkdir -p ${PACKAGE_DIR}/lib/modules/5.10.92-v8+/kernel/drivers/media/i2c/
 	install -p -m 644 ./pi/5.10.92-v7+/veyecam2m.ko  "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/media/i2c/"	
         install -p -m 644 ./pi/5.10.92-v7+/veyecam2m.dtbo "${PACKAGE_DIR}/boot/overlays/"
         install -p -m 644 ./pi/5.10.92-v7+/csimx307.ko  "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/media/i2c/"
