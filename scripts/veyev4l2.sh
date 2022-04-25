b3fd2f2e679f54ba60a47c54bb77db1e6dab5ebc
Raphael Scholle <raphael.scholle@uni-wuppertal.de>
2022-04-25 15:28:38 +0200

    put right version information in the kernel, change veye to the correct folders

--- scripts/veyev4l2.sh
+++ scripts/veyev4l2.sh
@@ -19,17 +19,17 @@ function build_veyev4l2_driver() {
         mkdir -p ${PACKAGE_DIR}/lib/modules/5.10.92-v7+/kernel/drivers/media/i2c/
         mkdir -p ${PACKAGE_DIR}/lib/modules/5.10.92-v7l+/kernel/drivers/media/i2c/
         mkdir -p ${PACKAGE_DIR}/lib/modules/5.10.92-v8+/kernel/drivers/media/i2c/
-        install -p -m 644 ./pi/5.10.92-v7+/veyecam2m.ko  "${PACKAGE_DIR}/lib/modules/5.10.92-v7+/kernel/drivers/media/i2c/"
+		
         install -p -m 644 ./pi/5.10.92-v7+/veyecam2m.dtbo "${PACKAGE_DIR}/boot/overlays/"
-        install -p -m 644 ./pi/5.10.92-v7+/csimx307.ko  "${PACKAGE_DIR}/lib/modules/5.10.92-v7+/kernel/drivers/media/i2c/"
+        install -p -m 644 ./pi/5.10.92-v7+/csimx307.ko  "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/media/i2c/"
         install -p -m 644 ./pi/5.10.92-v7+/csimx307.dtbo "${PACKAGE_DIR}/boot/overlays/"
-        install -p -m 644 ./pi/5.10.92-v7l+/veyecam2m.ko  "${PACKAGE_DIR}/lib/modules/5.10.92-v7l+/kernel/drivers/media/i2c/"
+        install -p -m 644 ./pi/5.10.92-v7l+/veyecam2m.ko  "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/media/i2c/"
         install -p -m 644 ./pi/5.10.92-v7l+/veyecam2m.dtbo "${PACKAGE_DIR}/boot/overlays/"
-        install -p -m 644 ./pi/5.10.92-v7l+/csimx307.ko  "${PACKAGE_DIR}/lib/modules/5.10.92-v7l+/kernel/drivers/media/i2c/"
+        install -p -m 644 ./pi/5.10.92-v7l+/csimx307.ko  "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/media/i2c/"
         install -p -m 644 ./pi/5.10.92-v7l+/csimx307.dtbo "${PACKAGE_DIR}/boot/overlays/"
-        install -p -m 644 ./pi/5.10.92-v8+/veyecam2m.ko  "${PACKAGE_DIR}/lib/modules/5.10.92-v8+/kernel/drivers/media/i2c/"
+        install -p -m 644 ./pi/5.10.92-v8+/veyecam2m.ko  "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/media/i2c/"
         install -p -m 644 ./pi/5.10.92-v8+/veyecam2m.dtbo "${PACKAGE_DIR}/boot/overlays/"
-        install -p -m 644 ./pi/5.10.92-v8+/csimx307.ko  "${PACKAGE_DIR}/lib/modules/5.10.92-v8+/kernel/drivers/media/i2c/"
+        install -p -m 644 ./pi/5.10.92-v8+/csimx307.ko  "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/media/i2c/"
         install -p -m 644 ./pi/5.10.92-v8+/csimx307.dtbo "${PACKAGE_DIR}/boot/overlays/"
         /sbin/depmod -a 5.10.92-v7+
         /sbin/depmod -a 5.10.92-v7l+
