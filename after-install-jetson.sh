afbfc4dcf71f7171afe0228ad97bc2ddac7a8766
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-08-14 16:12:24 +0200

    Update after-install-jetson.sh

--- after-install-jetson.sh
+++ after-install-jetson.sh
@@ -7,12 +7,12 @@ mv /boot/kernel.img /boot/Image
 
 if test -f "/usr/local/share/openhd/Jetson-2GB"; then 
    echo "you have a jetson 2GB model"
-   cp -a /usr/local/share/openhd/kernel/kernel_tegra210-p3448-0003-p3542-0000.dtb /boot/dtb/
+   cp -a /usr/local/share/openhd/kernel/tegra210-p3448-0003-p3542-0000.dtb /boot/dtb/
 fi
 
 if test -f "/usr/local/share/openhd/Jetson-4GB"; then 
    echo "you have a jetson 4GB model"
-   cp -a /usr/local/share/openhd/kernel/kernel_tegra210-p3448-0000-p3449-0000-b00.dtb /boot/dtb/
+   cp -a /usr/local/share/openhd/kernel/tegra210-p3448-0000-p3449-0000-b00.dtb /boot/dtb/
 fi
 
 depmod -a
