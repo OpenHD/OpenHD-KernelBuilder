245dabc8cf486c0ede0fd8d8718e331bd94639d3
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-09-19 23:48:59 +0200

    Update after-install-jetson.sh

--- after-install-jetson.sh
+++ after-install-jetson.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
-mount -oremount,rw /boot
-
-
 mkdir -p /boot/overlays
 
 cp -a /usr/local/share/openhd/kernel/* /boot/
 mv /boot/kernel.img /boot/Image
 
-   sudo rm /boot/dtb/*
-   sudo rm /boot/*.dtb
+   sudo rm -f /boot/dtb/*
+   sudo rm -f /boot/*.dtb
    mkdir /boot/veyecam/
    cp -a /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0003-p3542-0000.dtb /boot/veyecam/
    cp -a /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0000-p3449-0000-b00.dtb /boot/veyecam/
    cp -a /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0000-p3449-0000-a02.dtb /boot/veyecam/
 
 depmod -a
-
-mount -oremount,ro /boot
