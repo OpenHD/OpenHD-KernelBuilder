88c9da78499a00ecff9ad451ff98048f6b9a9725
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-11-10 22:01:49 +0100

    Update realtek.sh

--- scripts/realtek.sh
+++ scripts/realtek.sh
@@ -34,7 +34,7 @@ function fetch_rtl8812au_driver() {
 }
 
 function build_rtl8812au_driver() {
-    pushd rtl8812au-5.2.20
+    pushd rtl8812au
         make clean
 	
 	if [[ "${PLATFORM}" == "pi" ]]; then
