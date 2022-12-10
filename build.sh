5c3d74b4194aedd5d1922b753dd48c14494d099a
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-12-10 02:25:00 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -112,18 +112,18 @@ build_pi_kernel() {
         sed -i '283 i veye_mvcam-overlay.dts \\' ${LINUX_DIR}/arch/arm/boot/dts/overlays/Makefile
         sed -i '284 i veyecam2m-overlay.dts \\' ${LINUX_DIR}/arch/arm/boot/dts/overlays/Makefile
         echo "Set Overlays"
+        
         # yes "" | make oldconfig || exit 1
             if [[ "${ISA}" == "v7l" ]]; then
                 make clean
                 make bcm2711_defconfig
             elif [[ "${ISA}" == "v7" ]]; then
-            echo "debug"
-            #    make clean
-            #    make bcm2709_defconfig
+                make clean
+                make bcm2709_defconfig
             elif [[ "${ISA}" == "v6" ]]; then
             echo "debug"
-            #    make clean
-            #    make bcmrpi_defconfig
+                make clean
+                make bcmrpi_defconfig
             # currently only doing default config, modified config can follow later, but standart eases the possibility to upgrade to a newer kernel 
             fi
         KERNEL=${KERNEL} KBUILD_BUILD_TIMESTAMP='' make -j $J_CORES zImage modules dtbs || exit 1
