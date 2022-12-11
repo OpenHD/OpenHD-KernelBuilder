0abbf0961433ba47a9b490b02d39947b6b661c43
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2022-12-11 17:04:03 +0100

    fix so that all modules are copied

--- build.sh
+++ build.sh
@@ -285,7 +285,7 @@ if [[ "${PLATFORM}" == "pi" ]]; then
         #copy drivers, not copying the makefile (the makefile will make the kernel not build)
         cp -r $RELEASE_PACK_DIR/driver_source/cam_drv_src/rpi-5.15_all/*.c workdir/linux-pi/drivers/media/i2c/
         cp -r $RELEASE_PACK_DIR/driver_source/cam_drv_src/rpi-5.15_all/*.h workdir/linux-pi/drivers/media/i2c/
-        echo 'obj-m := veye_mvcam.o veyecam2m.o csimx307.o cssc132.o' >> workdir/linux-pi/drivers/media/i2c/Makefile
+        echo 'obj-m += veye_mvcam.o veyecam2m.o csimx307.o cssc132.o' >> workdir/linux-pi/drivers/media/i2c/Makefile
         cp -r additional/Kconfig workdir/linux-pi/drivers/media/i2c/
         #copying the dts-files
         cp -r $RELEASE_PACK_DIR/driver_source/dts/rpi-5.15.y/* workdir/linux-pi/arch/arm/boot/dts/overlays/
