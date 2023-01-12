56f0f8dc3f182df0e6c78b5dfcbce3e7ab61d0f0
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-01-12 21:27:11 +0100

    dtb-fixes for jetson

--- after-install-jetson.sh
+++ after-install-jetson.sh
@@ -8,6 +8,6 @@ mv /boot/kernel.img /boot/Image
    sudo rm -f /boot/dtb/*
    sudo rm -f /boot/*.dtb
    mkdir -p /boot/veyecam/
-   cp -arf /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0003-p3542-0000.dtb /boot/veyecam/
-   cp -arf /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0000-p3449-0000-b00.dtb /boot/veyecam/
-   cp -arf /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0000-p3449-0000-a02.dtb /boot/veyecam/
+   cp -arf /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0003-p3542-0000.dtb /boot/dtb/
+   cp -arf /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0000-p3449-0000-b00.dtb /boot/dtb/
+   cp -arf /usr/local/share/openhd/kernel/veyecam/tegra210-p3448-0000-p3449-0000-a02.dtb /boot/dtb/
