4abe369c14848b54f688ef7657c852714cb35b33
Raphael Scholle <68374617+raphaelscholle@users.noreply.github.com>
2023-03-12 00:31:41 +0100

    dirty test

--- scripts/common.sh
+++ scripts/common.sh
@@ -94,30 +94,10 @@ function package_headers() {
     # to separate them, and it would create a bit of overhead to manage it that way.
     #
 
-    if [[ "${ONLINE}" == "ONLINE" ]]; then
-       
-	if [[ "${PLATFORM}" == "jetson" ]]; then
-	    git describe --exact-match HEAD >/dev/null 2>&1
-            echo "Pushing package to OpenHD repository"
-            cloudsmith push deb openhd/openhd-2-3-evo/ubuntu/${DISTRO} ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb
-        fi
-        if [[ "${PLATFORM}" == "rock5" ]]; then
-	    git describe --exact-match HEAD >/dev/null 2>&1
+ 	    git describe --exact-match HEAD >/dev/null 2>&1
             echo "Pushing package to OpenHD repository for rock5"
             cloudsmith push deb openhd/openhd-2-3-evo/debian/bullseye ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb
-        fi
-
-
-        if [[ $? -eq 0 ]]; then
-	    git describe --exact-match HEAD >/dev/null 2>&1
-            echo "Pushing package to OpenHD 2.3 repository"
-            cloudsmith push deb openhd/openhd-2-3-evo/raspbian/${DISTRO} ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb
-        else
-	    git describe --exact-match HEAD >/dev/null 2>&1
-            echo "Pushing package to OpenHD 2.3 repository"
-            cloudsmith push deb openhd/openhd-2-3-evo/raspbian/${DISTRO} ${PACKAGE_NAME}_${VERSION}_${PACKAGE_ARCH}.deb
-        fi
-    fi
+       
 }
 
 
