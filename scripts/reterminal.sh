323eb3307abeea5aa8ac90a14c97f9a147f50503
Ka Ru <ka_ru2003@msn.com>
2023-12-30 20:45:32 +0100

    add reTerminal

--- scripts/reterminal.sh
+++ scripts/reterminal.sh
@@ -4,7 +4,7 @@ function fetch_reterminal_driver() {
 
     if [[ "${PLATFORM}" == "pi" ]]; then    
         echo "Download the reTerminal driver"
-        git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays || exit 1
+        # git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays || exit 1
         mkdir -p ${PACKAGE_DIR}/usr/local/share/reterminal || exit 1
         cp -rf seeed-linux-dtoverlays/modules/seeed-voicecard/wm8960_asound.state ${PACKAGE_DIR}/usr/local/share/reterminal/wm8960_asound.state || exit 1
         cp -rf seeed-linux-dtoverlays/modules/seeed-voicecard/asound_2mic.conf ${PACKAGE_DIR}/usr/local/share/reterminal/asound_2mic.conf || exit 1
@@ -13,12 +13,14 @@ function fetch_reterminal_driver() {
 }
 
 function build_reterminal_driver() {
+    echo "Build the reTerminal driver"
+    export KVERSION=${KERNEL_VERSION}
 
     if [[ "${PLATFORM}" == "pi" ]]; then
         pushd seeed-linux-dtoverlays/modules
         for DRIVER in mipi_dsi ltr30x lis3lv02d bq24179_charger; do
             pushd $DRIVER
-            make clean || exit 1
+            # make clean || exit 1
             make -C ${LINUX_DIR} KSRC=${LINUX_DIR} -j $J_CORES M=$(pwd) modules || exit 1
 	        mkdir -p ${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/reterminal || exit 1
             install -p -m 644 *.ko "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/reterminal/" || exit 1
