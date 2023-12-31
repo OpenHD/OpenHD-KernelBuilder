5db28df866be57b2e1a1cd55326c213c022aed81
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2023-12-31 02:10:07 +0100

    Update realtek.sh

--- scripts/realtek.sh
+++ scripts/realtek.sh
@@ -44,16 +44,6 @@ function build_rtl8812au_driver() {
     
         fi
 	
-	if [[ "${PLATFORM}" == "jetson" ]]; then
-		export KERNEL_VERSION="4.9.253OpenHD-2.1-tegra"
-		export CROSS_COMPILE=$Tools/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
-	        make KSRC=${LINUX_DIR}/build -j $J_CORES M=$(pwd) modules || exit 1
-		mkdir -p ${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl8812au || exit 1
-		rm $SRC_DIR/workdir/Linux_for_Tegra/source/public/kernel/nvidia/drivers/net/wireless/realtek/rtl8812au/rtl8812au.ko
-         	install -p -m 644 88XXau_wfb.ko "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl8812au/rtl8812au.ko" || exit 1
-	fi
-
-
        popd
 }
 
@@ -96,7 +86,7 @@ function build_rtl8812bu_driver() {
 	        fi
 
         mkdir -p ${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl88x2bu || exit 1
-        install -p -m 644 88x2bu.ko "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl88x2bu/" || exit 1
+        install -p -m 644 88x2bu_ohd.ko "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl88x2bu/" || exit 1
         rm -Rf ${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl8xxxu
         echo "removed original realtek driver out of the rpi source"
     popd
@@ -130,7 +120,7 @@ function build_rtl8852bu_driver() {
        	make KSRC=${LINUX_DIR} -j $J_CORES M=$(pwd) modules || exit 1
 
         mkdir -p ${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl8852bu || exit 1
-        install -p -m 644 8852bu.ko "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl8852bu/" || exit 1
+        install -p -m 644 8852bu_ohd.ko "${PACKAGE_DIR}/lib/modules/${KERNEL_VERSION}/kernel/drivers/net/wireless/realtek/rtl8852bu/" || exit 1
     popd
 }
 
